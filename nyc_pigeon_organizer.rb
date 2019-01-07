c7d9067cf8db95f91edbefcac1833e1974625c7a
Redmond Chan <redmondchan92@gmail.com>
2019-01-07 03:29:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |describe, array|
+      array.each do |name|
+        if pigeon_list.has_key?(name)
+          if pigeon_list[name].has_key?(key)
+            pigeon_list[name][key] << describe.to_s
+          else
+            pigeon_list[name][key] = [describe.to_s]
+          end
+        else
+          pigeon_list[name] = {key => [describe.to_s]}
+        end
+      end
+    end
+  end
+  pigeon_list# write your code here!
 end
